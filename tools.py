caa5454ba5823e2c1e46c8f4ecdde2961270653c
haxor111111 <42025878+haxor111111@users.noreply.github.com>
2019-02-25 10:47:44 -0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,1 +1,25 @@
-class item():
+class Item:
+  def __init__(self, name, width, height, x_pos, y_pos, image_path)
+		object_image = pygame.image.load(image_path)
+		self.image = pygame.transform.scale(object_image, (width, height))
+		self.name = name
+		self.width = width
+		self.height = height
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		
+	def draw(self, background):
+    background.blit(self.image, (self.x_pos, self.y_pos))
+
+class Weapon(Item):
+	def __init__(self, name, damage, width, height, x_pos, y_pos, image_path)
+		self.damage = damage
+		super().__init__(name, width, height, x_pos, y_pos, image_path)
+		
+class HItem(Item):
+	def __init__(self, name, hp, width, height, x_pos, y_pos, image_path)
+		self.hp = hp
+		super().__init__(name, width, height, x_pos, y_pos, image_path)
+	
+	def heal(self, hp):
+		player_character.hp += self.hp
